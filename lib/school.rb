50c5dbed2e48654eb3035d36dde495c7eae351be
John Tuttle <thejwt@gmail.com>
2017-09-27 23:45:47 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -39,14 +39,3 @@ attr_reader :name, :grade
     end
 
 end
-
-temp_school = School.new("temp_school")
-
-temp_school.add_student("Homer Simpson", 9)
-temp_school.add_student("Bart Simpson", 9)
-temp_school.add_student("Avi Flombaum", 10)
-temp_school.add_student("Jeff Baird", 10)
-temp_school.add_student("Blake Johnson", 7)
-temp_school.add_student("Jack Bauer", 7)
-
-temp_school.sort
